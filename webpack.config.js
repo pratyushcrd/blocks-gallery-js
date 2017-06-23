84e93d135e1423b9c0a9f7733439c5953c14c80b
pratyushcrd <pratyush.creed@gmail.com>
2017-06-23 04:29:59 -0700

    Added webpack and configured build system

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'blocks-gallery.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "example"),
+    compress: true,
+    port: 80
+  }
+};
