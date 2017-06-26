a21848eff30fa7e6f936e9c709193dd6814e6ce0
pratyushcrd <pratyush.creed@gmail.com>
2017-06-26 07:32:36 -0700

    Added pluck function in library

--- src/lib.js
+++ src/lib.js
@@ -1,3 +1,19 @@
+/**
+ * Function to return first non 'undefined' value
+ * from arguments list
+ */
+const pluck = function () {
+    let i = 0,
+      ii = arguments.length,
+      item;
+    for (; i < ii; ++i) {
+      item = arguments[i];
+      if (item !== undefined) {
+        return item;
+      }
+    }
+  }
+
 export default {
-  
+  pluck: pluck 
 }
