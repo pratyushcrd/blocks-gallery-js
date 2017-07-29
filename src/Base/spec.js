f7a16c363e409e8a3f9f64589676c9e6a940e6c8
pratyushcrd <pratyush.creed@gmail.com>
2017-07-29 09:44:02 -0700

    Added test for Base Component

--- src/Base/spec.js
+++ src/Base/spec.js
@@ -4,11 +4,22 @@ describe('Base Component Test', () => {
     isEnvVariable: true
   }
   const base = new Base(envOb)
+  const child = new Base(base)
+  
   it('should have a constructor that receives env object', () => {
     expect(base.environment).to.equal(envOb)
   })
 
   it('should have methods addToEnv and getFromEnv', () => {
     expect(base.addToEnv).to.be.a('function')
+  })
+
+  it('should share same enviroment bwtween parent and child', () => {
+    expect(base.environment).to.equal(child.environment)
+  })
+
+  it('should have a different store for base and child', () => {
+    base.addToStore('x', 1)
+    expect(child.getFromStore('x')).to.equal(undefined)
   })
 })
