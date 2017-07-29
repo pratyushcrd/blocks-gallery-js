d8694d1e62842003b9de12fd573d332f22055439
pratyushcrd <pratyush.creed@gmail.com>
2017-07-29 08:27:50 -0700

    Added tests for base

--- src/Base/spec.js
+++ src/Base/spec.js
@@ -1,10 +1,14 @@
 import Base from './'
 describe('Base Component Test', () => {
+  const envOb = {
+    isEnvVariable: true
+  }
+  const base = new Base(envOb)
   it('should have a constructor that receives env object', () => {
-    const envOb = {
-      isEnvVariable: true
-    }
-    const base = new Base(envOb)
     expect(base.environment).to.equal(envOb)
+  })
+
+  it('should have methods addToEnv and getFromEnv', () => {
+    expect(base.addToEnv).to.be.a('function')
   })
 })
