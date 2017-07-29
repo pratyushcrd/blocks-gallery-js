3070131783ea3cc72f174289623c7a7a4c4fc2d7
pratyushcrd <pratyush.creed@gmail.com>
2017-07-29 10:21:41 -0700

    Constructor added for Blocks manager

---
+++ src/Blocks/index.js
@@ -0,0 +1,26 @@
+import Base from '../Base'
+import Grid from '../Grid/'
+
+/**
+ * Component responsible for blocks management of image,
+ * responsible for querying image and show / hide or animate
+ * when required
+ */
+class Blocks extends Base {
+  constructor(parent) {
+    super(parent)
+
+    const rootEl = this.getFromEnv('rootEl')
+    const blocksEl = document.createElement('div')
+
+    // Append child to rootEl
+    rootEl.appendChild(blocksEl)
+
+    // Add blocks node to store
+    this.addToStore('blocksEl', blocksEl)
+    // Initialize grid for the block instance
+    this.addToStore('grid', new Grid(this, rootEl))
+  }
+}
+
+export default Blocks
